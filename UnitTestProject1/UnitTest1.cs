eb8be5ce47569836b17e4bbbc3969e61773b0024
Александр Тихонов <oren.andrey@gmail.com>
2019-01-14 17:07:28 +0500

    Билибамба

---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using fraction;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void Add_12and13_56()
+        {
+            //arrange
+            Fraction a = new Fraction(1, 2);
+            Fraction b = new Fraction(1, 3);
+            Fraction expected = new Fraction(5, 6);
+            //act
+            Fraction actual = Fraction.Add(a, b);
+            //assert
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void Sub_12and13_16()
+        {
+            //arrange
+            Fraction a = new Fraction(1, 2);
+            Fraction b = new Fraction(1, 3);
+            Fraction expected = new Fraction(1, 6);
+            //act
+            Fraction actual = Fraction.Sub(a, b);
+            //assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+
+        public void SetFormat_24_12()
+        {
+            //arrange
+            Fraction a = new Fraction(2, 4);
+            Fraction expected = new Fraction(1, 2);
+            //act
+            Fraction actual = Fraction.SetFormat(a);
+            //assert
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void GetDouble_12()
+        {
+            //arrange
+            Fraction a = new Fraction(1, 2);
+            double expected = 0.5;
+
+
+            //act
+            double actual = Fraction.GetDouble(a);
+
+            //assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+
+        public void Mul_12and12_14()
+        {
+            //arrange
+            Fraction a = new Fraction(1,2);
+            Fraction b = new Fraction(1,2);
+            Fraction expected = new Fraction(1,4);
+            //act
+            Fraction actual = Fraction.Mul(a,b);
+            //assert
+            Assert.AreEqual(expected,actual);
+        }
+    }
+}
---
+++ fraction/Class1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fraction
+{
+    public class Fraction
+    {
+        int numerator;
+        int denominator;
+
+        public Fraction(int numerator, int denominator)
+        {
+            this.numerator = numerator;
+            this.denominator = denominator;
+        }
+
+        public static Fraction Add(Fraction a, Fraction b)
+        {
+            Fraction c = new Fraction(a.numerator * b.denominator + a.denominator * b.numerator,
+                a.denominator * b.denominator);
+            return c;
+        }
+
+        public static Fraction Sub(Fraction a, Fraction b)
+        {
+            Fraction c = new Fraction(a.numerator * b.denominator - a.denominator * b.numerator,
+                a.denominator * b.denominator);
+            return c;
+        }
+
+        public static Fraction SetFormat(Fraction t)
+        {
+            double max = 0;
+
+
+            if (t.numerator > t.denominator)
+                max = Math.Abs(t.denominator);
+            else
+                max = Math.Abs(t.numerator);
+            for (double i = max; i >= 2; i--)
+            {
+                if ((t.numerator % i == 0) & (t.denominator % i == 0))
+                {
+                    t.numerator = Convert.ToInt32(t.numerator / i);
+                    t.denominator = Convert.ToInt32(t.denominator / i);
+                }
+
+            }
+            return t;
+        }
+        public static double GetDouble(Fraction a)
+        {
+            double d = (double)a.numerator / (double)a.denominator;
+            string s = String.Format("{0:#.##}", d);
+
+            return double.Parse(s);
+        }
+
+        public static Fraction Mul(Fraction a, Fraction b)
+        {
+            Fraction c = new Fraction(a.numerator * b.numerator, a.denominator * b.denominator);
+            return c;
+
+        }
+
+        public override bool Equals(object obj)
+        {
+            var toCompareWith = obj as Fraction;
+            if (toCompareWith == null)
+                return false;
+            return this.numerator == toCompareWith.numerator &&
+                   this.denominator == toCompareWith.denominator;
+        }
+    }
+}
